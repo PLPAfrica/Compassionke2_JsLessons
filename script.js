ed63fde55132941bd4a7587731fb51dc09515442
Evans-mutuku <evansnyamai98@gmail.com>
2023-09-26 20:46:26 +0300

    first Js Lesson

---
+++ script.js
@@ -0,0 +1,28 @@
+const hello = "Evans";
+
+var fullName = "Evans Mutuku";
+let gender = "Male";
+const age = 25;
+const experience = true;
+
+console.log(fullName);
+console.log(gender);
+console.log(age, experience);
+
+// Objects Lesson
+
+const person = {
+  age: 45,
+  name: "John Doe",
+  weight: 77,
+};
+
+console.log(person);
+console.log(person.name);
+
+//  Arrays in Javascript
+
+let studentDetails = ["George", "Sophie", "Habibi", "Dorcas", 49];
+console.log(studentDetails);
+
+console.log(studentDetails[1]);
